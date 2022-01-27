e3ccc0e610d0e61b027e0fd7f78ff8ab37694886
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-27 18:58:17 +0000

    fixed login issues and registration issues

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 
 const generateRandomString = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -17,7 +17,6 @@ const generateRandomString = () => {
   return rString;
 };
 
-app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -26,12 +25,58 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const addUser = (email, password) => {
+  const id = generateRandomString();
+  users[id] = {
+    id,
+    email,
+    password
+  };
+  return id;
+};
+
+const findUser = email => {
+  return Object.values(users).find(user => user.email === email);
+};
+
+const checkPassword = (user, password) => {
+  if (user.password === password) {
+    return true;
+  } else { 
+    return false;
+  }
+};
+
+const checkRegistration = (email, password) => {
+  if (email && password) {
+    return true;
+  }
+  return false
+};
+
+const checkEmail = email => {
+  return Object.values(users).find(user => user.email === email);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
@@ -45,20 +90,20 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = { 
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL] 
   };
@@ -68,6 +113,30 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_login", templateVars);
+});
+
+app.get("/register", (req,res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_register", templateVars);
+});
+
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  if (!checkRegistration(email, password)) {
+    res.status(400).send('Email and/or password is missing');
+  } else if (findUser(email)) {
+    res.status(400).send('This email has already been registered')
+  } else {
+    const user_id = addUser(email, password);
+    res.cookie('user_id', user_id);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls", (req, res) => {
@@ -90,14 +159,23 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req,res) => {
-  res.cookie('username', req.body.username);
-  res.redirect("/urls");
+  const { email, password } = req.body;
+  const user = findUser(email);
+  if (!user) {
+    res.status(403).send("Email cannot be found");
+  } else if (!checkPassword(user, password))  {
+    res.status(403).send("Wrong password");
+  } else {
+    res.cookie('user_id', user.id);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
