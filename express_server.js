191b45f2ed8590726a2a0e617f883c292c36c8dc
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-28 02:11:29 +0000

    Added get user by email, encrypted cookies, made a helper module

--- express_server.js
+++ express_server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
+const cookieSession = require('cookie-session');
+const getUserByEmail = require('./helpers');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['e1d50c4f-538a-4682-89f4-c002f10a59c8', '2d310699-67d3-4b26-a3a4-1dbf2b67be5c'],
+  })
+);
+
 app.set("view engine", "ejs");
 
 
@@ -21,8 +28,6 @@ const generateRandomString = () => {
 
   return rString;
 };
-
-
 
 const urlDatabase = {
   "b2xVn2": { 
@@ -47,25 +52,14 @@ const users = {
 };
 
 const addUser = (email, password) => {
+  const hashedPassword = bcrypt.hashSync(password, 10);
   const id = generateRandomString();
   users[id] = {
     id,
     email,
-    password
+    password: hashedPassword
   };
   return id;
-};
-
-const findUser = email => {
-  return Object.values(users).find(user => user.email === email);
-};
-
-const checkPassword = (user, password) => {
-  if (user.password === password) {
-    return true;
-  } else { 
-    return false;
-  }
 };
 
 const userUrls = (id) => {
@@ -78,15 +72,10 @@ const userUrls = (id) => {
   return fUrls;
 };
 
-const checkEmail = email => {
-  return Object.values(users).find(user => user.email === email);
-}
 
 //Gets
 
-app.get("/urls2.json", (req, res) => {
-  res.json(users);
-});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -101,14 +90,18 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls2.json", (req, res) => {
+  res.json(users);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user_id"]],
-    urls: userUrls(req.cookies["user_id"])
+    user: users[req.session.user_id],
+    urls: userUrls(req.session.user_id)
   };
   if (templateVars.user) {
     res.render("urls_index", templateVars);
@@ -118,7 +111,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.user_id] };
   if (templateVars.user) {
     res.render("urls_new", templateVars);
   } else {
@@ -128,14 +121,11 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL].longURL
   };
-  if (!templateVars.user) {
-    res.status(400).send("You need to register or login to access this page");
-  }
-  if (req.cookies["user_id"] === urlDatabase[templateVars.shortURL].userID) {
+  if (req.session.user_id === urlDatabase[templateVars.shortURL].userID) {
     res.render("urls_show", templateVars);
   } else {
     res.status(400).send("This TinyURL doesn't belong to you!");
@@ -148,12 +138,12 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.user_id] };
   res.render("urls_login", templateVars);
 });
 
 app.get("/register", (req,res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.user_id] };
   res.render("urls_register", templateVars);
 });
 
@@ -163,18 +153,18 @@ app.post("/register", (req, res) => {
   const { email, password } = req.body;
   if (!(email || !password)) {
     res.status(400).send('Email and/or password is missing');
-  } else if (findUser(email)) {
+  } else if (getUserByEmail(email, users)) {
     res.status(400).send('This email has already been registered')
   } else {
     const user_id = addUser(email, password);
     res.cookie('user_id', user_id);
-    res.redirect("/urls");
+    res.redirect("/login");
   }
 });
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
-  const userID = req.cookies['user_id'];
+  const userID = req.session.user_id;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = { longURL, userID };
   res.redirect(`/urls/${shortURL}`);
@@ -182,7 +172,7 @@ app.post("/urls", (req, res) => {
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
-  if (req.cookies["user_id"] === urlDatabase[shortURL].userID) {
+  if (req.session.user_id === urlDatabase[shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   } else {
@@ -193,7 +183,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  if (req.cookies["user_id"] === urlDatabase[shortURL].userID) {
+  if (req.session.user_id === urlDatabase[shortURL].userID) {
     urlDatabase[shortURL].longURL = longURL;
     res.redirect(`/urls/${shortURL}`);
   } else {
@@ -203,20 +193,20 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.post("/login", (req,res) => {
   const { email, password } = req.body;
-  const user = findUser(email);
+  const user = getUserByEmail(email, users);
   if (!user) {
     res.status(403).send("Email cannot be found");
-  } else if (!checkPassword(user, password))  {
+  } else if (!bcrypt.compareSync(password, user.password))  {
     res.status(403).send("Wrong password");
   } else {
-    res.cookie('user_id', user.id);
+    req.session.user_id = user.id;
     res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect("/urls");
+  req.session = null;
+  res.redirect("/login");
 });
 
 
---
+++ helpers.js
@@ -0,0 +1,6 @@
+
+const getUserByEmail = (email, database) => {
+  return Object.values(database).find(user => user.email === email);
+};
+
+module.exports = getUserByEmail;
